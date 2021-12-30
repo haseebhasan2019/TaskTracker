9d143f356c6d31ec024d49735c2babd71de83c8e
haseebhasan2019 <haseebhasan2019@gmail.com>
2021-12-29 21:48:00 -0500

    Day 1 Progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    },
+    {
+      "id": 3,
+      "text": "Grocery Shopping",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": false
+    },
+  ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    // console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  //Add Task
+  const addTask = (task) => {
+    console.log(task);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="placeholder" />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask}
+          onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Do'
+      )}
     </div>
   );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
